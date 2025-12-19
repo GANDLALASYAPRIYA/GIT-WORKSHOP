d09e9e719051ab625d81c9d1d5f3b15025b5fa4a
GANDLA LASYA PRIYA <lasyapriyagandla@gmail.com>
2025-12-19 17:12:41 +0530

    made changes

---
+++ demo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Demo
+{
+	void display()
+	{
+		int num=10;
+		System.out.println("num="+num);
+	}
+	public static void main(String args[])
+	{
+		Demo d=new Demo();
+		d.display();
+	}
+	
+}
+
